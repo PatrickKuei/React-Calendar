76baf37f383c85c7f214681cc1b52ba2f7fba7dd
PatrickYang <azureroki@gmail.com>
2020-04-27 20:37:19 +0800

    Updates

---
+++ precache-manifest.8c05a635369a31429234abd5d3bdb9f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cff007ee5f527456251230e4b7c76d07",
+    "url": "/'https://github.com/PatrickKuei/React-Calendar'/index.html"
+  },
+  {
+    "revision": "375860403368802bafac",
+    "url": "/'https://github.com/PatrickKuei/React-Calendar'/static/css/main.a4fdd934.chunk.css"
+  },
+  {
+    "revision": "dba0937b70d08338ad39",
+    "url": "/'https://github.com/PatrickKuei/React-Calendar'/static/js/2.8067e9bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/'https://github.com/PatrickKuei/React-Calendar'/static/js/2.8067e9bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "375860403368802bafac",
+    "url": "/'https://github.com/PatrickKuei/React-Calendar'/static/js/main.e08b34d5.chunk.js"
+  },
+  {
+    "revision": "374e23a33e526cfd71b2",
+    "url": "/'https://github.com/PatrickKuei/React-Calendar'/static/js/runtime-main.157b5728.js"
+  },
+  {
+    "revision": "5e98861f6ef1ce225121abfb3f418166",
+    "url": "/'https://github.com/PatrickKuei/React-Calendar'/static/media/getFetch.5e98861f.cjs"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/'https://github.com/PatrickKuei/React-Calendar'/precache-manifest.8c05a635369a31429234abd5d3bdb9f4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/'https://github.com/PatrickKuei/React-Calendar'/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
